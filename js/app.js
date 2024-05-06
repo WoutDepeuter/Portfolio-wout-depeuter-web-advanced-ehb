cd38018aaebaa6d24e5f3d286e39eafb1b5a9636
WoutDepeuter <123627621+WoutDepeuter@users.noreply.github.com>
2024-05-06 19:57:24 +0200

    Aanmaak login pagina nog geen echt functionaliteiten

--- js/app.js
+++ js/app.js
@@ -1,28 +1,3 @@
 'use strict';
 
-let addButton = document.getElementById("Add");
-let removeButton = document.getElementById("Remove");
 
-addButton.addEventListener("click", addTask);
-removeButton.addEventListener("click", removeTask);
-
-function addTask() {
-  // Get the element where you want to add the task
-  let tasksElement = document.getElementById("tasks");
-
-  let task = prompt("Enter task:");
-
-  let newTaskElement = document.createElement("li");
-
-  newTaskElement.textContent = task;
-
-  tasksElement.appendChild(newTaskElement);
-}
-
-
-function removeTask() {
-  // Get the tasks element
-  let tasksElement = document.getElementById("tasks");
-
-  tasksElement.innerHTML = "";
-}
